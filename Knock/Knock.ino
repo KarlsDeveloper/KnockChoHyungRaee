380debf76c1e567263a7174056de0e7dc854d7f9
Mr.Jo <hi91800@gmail.com>
2018-10-21 07:30:35 +0900

    changed 10/21/07:30

--- Knock/Knock.ino
+++ Knock/Knock.ino
@@ -7,7 +7,7 @@ LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27,16,2);  // 16 문자 및 2 라인
 
 void setup() {
   //Wire.begin();                // 화면에 나오게 하기
-  lcd.begin();             // lcd.begin() 수정했습니다.
+  lcd.begin();             // 가로16개, 세로2개를 초기화 
   lcd.home();                  // 홈의로보내어 초기화
   lcd.clear();                 // 액정초기화
   lcd.print("Welcome to ");  // 화면에 Hello Arduino출력
@@ -17,7 +17,5 @@ void setup() {
 
 void loop() {
   // 메인 코드를 여기에 넣어 반복적으로 실행하십시오.:
-  
-  //  코드를 수정하시오.
 
 }
